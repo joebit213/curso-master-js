678d33e7bbc09250ae9db9dd5ac844ec7ae9783c
joseph <josephfguillen@gmail.com>
2020-03-09 10:55:38 -0600

    Patrones de diseño

--- js-jquery/Basic/03-textos.js
+++ js-jquery/Basic/03-textos.js
@@ -21,4 +21,19 @@ function reloadLinks() {
 
     $(this).text(enlace)
   })
+
+
+  $('#tabs li a').click(function(){
+    var t = $(this).attr('id');
+  
+    if($(this).hasClass('inactive')){ //this is the start of our condition 
+      $('#tabs li a').addClass('inactive');           
+      $(this).removeClass('inactive');
+  
+      $('.container').hide();
+      $('#'+ t + 'C').fadeIn('slow');
+   }
+  });
+
+
 }
